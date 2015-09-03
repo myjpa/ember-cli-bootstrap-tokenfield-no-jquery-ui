dd6721429e6335522a0947525925a20aca5abbf9
Hao Liu <haoliu@nvidia.com>
2015-09-03 14:46:15 -0700

    removing jquery-ui dependency, expecting not enable jquery-ui style

--- blueprints/ember-cli-bootstrap-tokenfield/index.js
+++ blueprints/ember-cli-bootstrap-tokenfield/index.js
@@ -21,9 +21,7 @@ module.exports = {
     console.log('      Use that version or higher.');
 
     return _this.addBowerPackageToProject('jquery', '2.1.0').then(function() {
-      return _this.addBowerPackageToProject('jquery-ui', '1.11.4').then(function() {
         return _this.addBowerPackageToProject('bootstrap-tokenfield');
-      });
     });
   }
 };
